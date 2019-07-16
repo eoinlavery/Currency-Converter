7f14cf21cc7f9abcb8f9f40b94753e9e8ffad310
Eoin Lavery <dev@tenantc.co.uk>
2019-07-16 22:26:46 +0100

    Initial Upload

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Declare outlets for labels and buttons
+    @IBOutlet weak var cadButton: UIButton!
+    @IBOutlet weak var pesoButton: UIButton!
+    @IBOutlet weak var currencyToLabel: UILabel!
+    @IBOutlet weak var currencyFromTextField: UITextField!
+    @IBOutlet weak var currencyToTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    //Define CurrencyType Enum and  ddeclare variable to store current state
+    var currencyType: CurrencyType?
+    enum CurrencyType {
+        case cad
+        case peso
+    }
+    
+    //Decalre functions for buttons and conversion
+    @IBAction func convertButtonPressed(_ sender: Any) {
+        
+        guard let dollars: Double = Double(currencyFromTextField.text!),
+                let currencyType: CurrencyType = currencyType else {
+                    return
+                }
+        
+        //Define currencyFormatter
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = Locale(identifier: "en_US")
+        
+        currencyToTextField.text = currencyFormatter.string(from: NSNumber(value: convert(dollars: dollars, to: currencyType)))
+    }
+    
+    @IBAction func cadButtonPressed(_ sender: Any) {
+        currencyType = .cad
+        cadButton.isSelected = true
+        pesoButton.isSelected = false
+    }
+    
+    
+    @IBAction func pesoButtonPressed(_ sender: Any) {
+        currencyType = .peso
+        pesoButton.isSelected = true
+        cadButton.isSelected = false
     }
 
-
+    func convert(dollars: Double, to unit: CurrencyType) -> Double {
+        
+        //Declare variable for exchange rates
+        let cadRate: Double = 1.31
+        let pesoRate: Double = 19.10
+        
+        switch unit {
+        case .cad:
+            return dollars * cadRate
+        case .peso:
+            return dollars * pesoRate
+        }
+    }
 }
 
